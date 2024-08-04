6f4cc484c1a5d248b9336290236f628ac3ba7fed
estrela801 <rohenri0808@gmail.com>
2024-08-04 19:16:41 -0300

    mano muita coisa

--- server.js
+++ server.js
@@ -1,7 +1,57 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 const app = express()
-app.get('/usuarios', (req, res) =>{
-    res.send('ok, deu bom')
+
+
+app.use(express.json())
+app.get('/usuarios', async (req, res) =>{
+   const user =  await prisma.user.findMany()
+
+    res.status(200).json(user)
 })
 
+let users = []
+app.post('/usuarios',async (req,res)=>{
+    await prisma.user.create({
+        data:{
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
+
+    res.send('deu bom')
+})
+
+app.put('/usuarios/:var', async (req,res)=>{
+
+    console.log(req);
+    
+
+
+    await prisma.user.update({
+        where:{
+           id: req.params.var
+        },
+        data:{
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
+    res.send('deu bom')
+})
+
+app.delete('/usuario/:id', async (req,res) =>{
+    console.log(req);
+    
+    await prisma.user.delete({
+        where:{
+            id: req.params.id
+        }
+    })
+    res.status(200)
+})
 app.listen(3000)
